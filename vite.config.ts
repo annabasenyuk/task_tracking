6fc740e71fe4a39dd81982a0a470fcb652807d69
Anna Basenyuk <annabasenyuk@gmail.com>
2024-05-13 13:23:29 +0300

    some change

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/task_tracking/',
   plugins: [react()],
   resolve: {
     alias: [{ find: "@", replacement: "/src" }]
